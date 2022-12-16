d6e062c98f06a75ea7ea23f367120a885467ae65
YashSomalkar <yashsomalkar@gmail.com>
2022-12-17 04:26:58 +0530

    change_in_counsellor

--- lib/controller/data_controller.dart
+++ lib/controller/data_controller.dart
@@ -3,10 +3,12 @@ import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:untvoice/controller/comman_dailog.dart';
-class DataController extends GetxController {
+import 'package:untvoice/models/anonymous_user_model.dart';
 
+class DataController extends GetxController {
   final firebaseInstance = FirebaseFirestore.instance;
-  Map dataNotifier ={'first': '', 'last': ''};
+  Map dataNotifier = {'first': '', 'last': ''};
+  List<Counsellor> counsellorData = [];
   FirebaseAuth auth = FirebaseAuth.instance;
 
   Future<void> getUserProfileData() async {
@@ -28,8 +30,6 @@ class DataController extends GetxController {
         newData['dob'] = response.docs[0]['dob'];
         newData['imageUrl'] = response.docs[0]['image'];
         dataNotifier = newData;
-
-
       }
       debugPrint(dataNotifier.toString());
       CommonDialog.hideLoading();
@@ -42,11 +42,52 @@ class DataController extends GetxController {
       CommonDialog.showErrorDialog();
       print(error);
     }
+  } // @override
 
-  }// @override
 //   void update([List<Object>? ids, bool condition = true]) {
 //     // TODO: implement update
 //     super.update(ids, condition);
 //   }
+  Future<void> getLoginUserProduct() async {
+    print("counsellorData YEs $counsellorData");
+    counsellorData = [];
+    try {
+      CommonDialog.showLoading();
+      final List<Counsellor> loadedCounsellors = [];
+      var response = await firebaseInstance
+          .collection('counsellor_anonymous')
+          .where('work', isEqualTo: 'Counsellor')
+          .get();
 
+      if (response.docs.isNotEmpty) {
+        for (var result in response.docs) {
+          print(result.data());
+          print("Counsellor ID  ${result.id}");
+          loadedCounsellors.add(Counsellor(
+            charges: result['charges'],
+            dob: result['dob'],
+            firstname: result['first'],
+            gender: result['gender'],
+            image: result['image'],
+            longDetails: result['longDetails'],
+            mobileNumber: result['mobileNumber'],
+            shortDetails: result['shortDetails'],
+            uid: result.id,
+            work: result['work'],
+          ));
+        }
+      }
+      counsellorData.addAll(loadedCounsellors);
+      update();
+      CommonDialog.hideLoading();
+    } on FirebaseException catch (e) {
+      CommonDialog.hideLoading();
+      CommonDialog.showErrorDialog();
+      print("Error $e");
+    } catch (error) {
+      CommonDialog.hideLoading();
+      CommonDialog.showErrorDialog();
+      print("error $error");
+    }
+  }
 }
